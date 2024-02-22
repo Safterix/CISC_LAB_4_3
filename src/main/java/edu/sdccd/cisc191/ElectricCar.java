35b0e9dddc2baea6ec1af617070d5df95290cd8e
Aleister Steadman <aleistersteadman@gmail.com>
2024-02-22 15:17:20 -0800

    Implemented UML

---
+++ src/main/java/edu/sdccd/cisc191/ElectricCar.java
@@ -0,0 +1,24 @@
+package edu.sdccd.cisc191;
+
+public class ElectricCar extends Car implements Chargeable
+{
+    private int batteryCharge;
+
+    public ElectricCar(String manufacturerName, int milesOnVehicle, int price, int numberOfSeats, Option[] options, int numberOfDoors)
+    {
+        super(manufacturerName, milesOnVehicle, price, numberOfSeats, options, numberOfDoors);
+        batteryCharge = 66;
+    }
+
+    @Override
+    public int getBatteryCharge()
+    {
+        return batteryCharge;
+    }
+
+    @Override
+    public void chargeUp()
+    {
+        batteryCharge = 100;
+    }
+}
